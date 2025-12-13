ffa262a2701f4d84f5d4a231f08101dd73ed29ee
merziyahp <merziyahpoonawala@gmail.com>
2025-12-13 14:47:40 -0500

    Added section ids
    Added section ids to site sections

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -22,33 +22,41 @@ export default function Home() {
     <div className="min-h-screen">
       <Navigation />
       
-      <NewHero />
-      
-      <ProfileSection />
-      
-      <About 
-        description={portfolioData.about.description}
-        principles={portfolioData.about.principles}
-      />
-      
-      <CareerHighlights 
-        items={portfolioData.highlights.items}
-      />
-      
-      <Skills 
-        categories={portfolioData.skills.categories}
-      />
-      
-      <CaseStudies 
-        studies={portfolioData.caseStudies.studies}
-      />
-      
-      <Writing
-        title={writingData.title}
-        description={writingData.description}
-        substackUrl={writingData.substackUrl}
-        featured={writingData.featured}
-      />
+      <section id="hero">
+        <NewHero />
+      </section>
+
+      <section id="profile">
+        <ProfileSection />
+      </section>
+
+      <section id="about">
+        <About
+          description={portfolioData.about.description}
+          principles={portfolioData.about.principles}
+        />
+      </section>
+
+      <section id="career-highlights">
+        <CareerHighlights items={portfolioData.highlights.items} />
+      </section>
+
+      <section id="skills">
+        <Skills categories={portfolioData.skills.categories} />
+      </section>
+
+      <section id="case-studies">
+        <CaseStudies studies={portfolioData.caseStudies.studies} />
+      </section>
+
+      <section id="writing">
+        <Writing
+          title={writingData.title}
+          description={writingData.description}
+          substackUrl={writingData.substackUrl}
+          featured={writingData.featured}
+        />
+      </section>
       
       <section id="side-projects" className="py-16 bg-gray-50">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -66,12 +74,14 @@ export default function Home() {
           </div>
         </div>
       </section>
-      
-      <PersonalInterests 
-        items={portfolioData.interests.items}
-      />
-      
-      <Contact />
+
+      <section id="personal-interests">
+        <PersonalInterests items={portfolioData.interests.items} />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
       
       <Footer />
     </div>
